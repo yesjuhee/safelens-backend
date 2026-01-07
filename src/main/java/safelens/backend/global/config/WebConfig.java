b4af2d2ee8b2c3985a13376a49255fe3b6fdc5ef
yesjuhee <juhee0924@g.skku.edu>
2026-01-07 16:21:01 +0900

    fix: 모든 도메인 CORS 허용

--- src/main/java/safelens/backend/global/config/WebConfig.java
+++ src/main/java/safelens/backend/global/config/WebConfig.java
@@ -10,12 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:8080",
-                        "http://localhost:8081",
-                        "http://192.168.68.194:8080/"
-                )
-                .allowedOriginPatterns("chrome-extension://*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
